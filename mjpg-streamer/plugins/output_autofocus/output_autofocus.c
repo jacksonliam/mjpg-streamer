c1a6f0979399efed37fa6f575b3b90ee09b668c1
tom_stoeveken <tom_stoeveken@a8dad676-403b-0410-b964-c43bdf40fe89>
2008-01-20 21:02:13 +0000

    * first working version of autofocus, pumps quite often but at least a start
    git-svn-id: https://mjpg-streamer.svn.sourceforge.net/svnroot/mjpg-streamer@49 a8dad676-403b-0410-b964-c43bdf40fe89

--- mjpg-streamer/plugins/output_autofocus/output_autofocus.c
+++ mjpg-streamer/plugins/output_autofocus/output_autofocus.c
@@ -84,6 +84,8 @@ void worker_cleanup(void *arg) {
   close(fd);
 }
 
+
+
 /******************************************************************************
 Description.: this is the main worker thread
               it loops forever, grabs a fresh frame and calculates focus
@@ -92,8 +94,8 @@ Return Value:
 ******************************************************************************/
 void *worker_thread( void *arg ) {
   int frame_size=0;
-  double sv=-1.0, previous_sv=-1.0, delta=100;
-  int direction=-1, focus=255, step=10;
+  double sv=-1.0, max_sv=100.0, delta=500;
+  int focus=255, step=10, max_focus=100, search_focus=1;
 
   if ( (frame = malloc(256*1024)) == NULL ) {
     OPRINT("not enough memory for worker thread\n");
@@ -116,13 +118,40 @@ void *worker_thread( void *arg ) {
     /* process frame */
     sv = getFrameSharpnessValue(frame, frame_size);
     DBG("sharpness is: %f\n", sv);
-    if ( ABS(sv-previous_sv) > delta ) {
-      DBG("need to adjust sharpness (focus: %d)\n", focus);
-      focus += direction*step;
-      focus = pglobal->in.cmd(IN_CMD_FOCUS_SET, focus);
+
+    if ( search_focus || (ABS(sv-max_sv) > delta) ) {
+      DBG("adjusting focus: %d\n", focus);
+
+      /* entered because focus changed */
+      if ( !search_focus ) {
+        DBG("starting to search for focus\n");
+        max_focus    = 255;
+        focus        = 255;
+        max_sv       = -1.0;
+        search_focus = 1;
+      }
+
+      if ( focus <= 0 ) {
+        focus = max_focus;
+        DBG("max focus found at: %d\n", max_focus);
+        search_focus = 0;
+      }
+
+      if ( search_focus ) {
+        if (sv > max_sv) {
+          /* sharpness is better then max now */
+          DBG("found better focus at: %d\n", focus);
+          max_focus = focus;
+          max_sv = sv;
+        }
+
+        focus = MIN(MAX(focus-step,0), 255);
+        DBG("decrement focus now to: %d\n", focus);
+        focus = pglobal->in.cmd(IN_CMD_FOCUS_SET, focus);
+      }
     }
 
-    if (delay > 0) {
+    if ( (delay > 0) && !search_focus ) {
       usleep(1000*delay);
     }
   }
