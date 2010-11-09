cd8fe84440492f53e99f8d4ffcfe411865c25ad4
martonmiklos <martonmiklos@a8dad676-403b-0410-b964-c43bdf40fe89>
2010-11-09 23:46:49 +0000

    removed unneccessary directory
    git-svn-id: https://mjpg-streamer.svn.sourceforge.net/svnroot/mjpg-streamer@126 a8dad676-403b-0410-b964-c43bdf40fe89

--- mjpg-streamer-unstable/mjpg-streamer/plugins/output_autofocus/processJPEG_onlyCenter.h
+++
@@ -1,4 +0,0 @@
-#define HUFF_EXTEND(x,s)  ((x) < (1<<((s)-1)) ? (x) + (((-1)<<(s)) + 1) : (x))
-
-// haven't done restart markers, but these don't seem to appear
-double getFrameSharpnessValue(unsigned char *data, int len);
