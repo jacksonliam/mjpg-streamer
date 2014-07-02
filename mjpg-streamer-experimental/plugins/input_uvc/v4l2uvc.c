26581a82de8964f0c41c8dbe9091a4947d3ec6c0
yagamy <yagamy@gmail.com>
2014-07-02 05:41:18 +0000

    fix bug when using v4l2-loopback devices with id more than 9, such as /dev/video10

--- mjpg-streamer-experimental/plugins/input_uvc/v4l2uvc.c
+++ mjpg-streamer-experimental/plugins/input_uvc/v4l2uvc.c
@@ -69,7 +69,7 @@ int init_videoIn(struct vdIn *vd, char *device, int width,
     vd->videodevice = (char *) calloc(1, 16 * sizeof(char));
     vd->status = (char *) calloc(1, 100 * sizeof(char));
     vd->pictName = (char *) calloc(1, 80 * sizeof(char));
-    snprintf(vd->videodevice, 12, "%s", device);
+    snprintf(vd->videodevice, (16 - 1), "%s", device);
     vd->toggleAvi = 0;
     vd->getPict = 0;
     vd->signalquit = 1;
