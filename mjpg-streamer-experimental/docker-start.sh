5d90c200e107f8c3e340fcb5c2b2fe73674e59ea
chunliu <chunliu@outlook.com>
2017-01-05 21:17:53 +0800

    containerization

---
+++ mjpg-streamer-experimental/docker-start.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+if [ "$1" = 'postgres' ]; then
+    chown -R postgres "$PGDATA"
+
+    if [ -z "$(ls -A "$PGDATA")" ]; then
+        gosu postgres initdb
+    fi
+
+    exec gosu postgres "$@"
+fi
+
+export LD_LIBRARY_PATH=.
+./mjpg_streamer "$@" 
