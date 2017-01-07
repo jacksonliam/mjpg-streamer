bac6b949f3130102ab803ac9587fc9cad5e63095
chunliu <chunliu@outlook.com>
2017-01-07 10:50:39 +0800

    Update the script with correct version.

--- mjpg-streamer-experimental/docker-start.sh
+++ mjpg-streamer-experimental/docker-start.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 set -e
 
-if [ "$1" = 'postgres' ]; then
-    chown -R postgres "$PGDATA"
-
-    if [ -z "$(ls -A "$PGDATA")" ]; then
-        gosu postgres initdb
-    fi
-
-    exec gosu postgres "$@"
-fi
-
 export LD_LIBRARY_PATH="/mjpg-streamer/mjpg-streamer-experimental"
 ./mjpg_streamer -o "$1" -i "$2"
