03baae6036c334c6f29f06e3ca9daf5fd22ba663
davralin <davralin@davralin.host>
2020-12-30 16:29:09 +0100

    Switch to a more recent docker image

--- mjpg-streamer-experimental/docker-start.sh
+++ mjpg-streamer-experimental/docker-start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-export LD_LIBRARY_PATH="/mjpg-streamer/mjpg-streamer-experimental"
+export LD_LIBRARY_PATH="/usr/local/src/mjpg-streamer-experimental"
 ./mjpg_streamer -o "$1" -i "$2"
