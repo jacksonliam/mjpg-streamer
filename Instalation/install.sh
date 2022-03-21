c00f17dd193f66802306c2269fcc7bbf27bed9f3
St3v3-B <stevebussing28@outlook.com>
2022-03-21 16:18:42 +0100

    update

--- Instalation/install.sh
+++ Instalation/install.sh
@@ -2,9 +2,9 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 echo "${RED}Starting install of Raspi Cam${NC}\n" # print that hthe install is starting
+cd ~ # Go to the home directory
 sudo rm -r mjpg-streamer/
 sudo rm /etc/systemd/system/raswebcamd.service
-cd ~ # Go to the home directory
 sudo apt install subversion libjpeg8-dev imagemagick ffmpeg libv4l-dev cmake git -y # Installing the dependacys that are needed to install the software
 git clone https://github.com/St3v3-B/mjpg-streamer.git # Download the files from github
 cd mjpg-streamer/mjpg-streamer-experimental # Go in to the directory that has been downloaded
@@ -14,3 +14,6 @@ echo "${RED}Making webcamDaemon executable ${NC}\n"
 sudo chmod +x mjpg_streamer/scripts/webcamDaemon
 echo "${RED}installing the raswebcamd service${NC}\n"
 sudo mv mjpg-streamer/Instalation/raswebcamd.service /etc/systemd/system/raswebcamd.service
+sudo systemctl daemon-reload
+sudo systemctl enable raswebcamd
+sudo systemctl start raswebcamd
