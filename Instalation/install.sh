3ee48a53162d3f4e96421825011a6187287a2d02
St3v3-B <stevebussing28@outlook.com>
2022-03-21 16:45:27 +0100

    Update install.sh

--- Instalation/install.sh
+++ Instalation/install.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
-RED='\033[0;31m'
-NC='\033[0m' # No Color
-echo "${RED}Starting install of Raspi Cam${NC}\n" # print that hthe install is starting
+echo "Starting install of Raspi Cam" # print that hthe install is starting
+
 cd ~ # Go to the home directory
+
+# starting remove of old service if present
+systemctl stop raswebcamd
+systemctl disable raswebcamd
+systemctl daemon-reload
 sudo rm -r mjpg-streamer/
 sudo rm /etc/systemd/system/raswebcamd.service
+# end removing old service 
+
 sudo apt install subversion libjpeg8-dev imagemagick ffmpeg libv4l-dev cmake git -y # Installing the dependacys that are needed to install the software
+
 git clone https://github.com/St3v3-B/mjpg-streamer.git # Download the files from github
+
 cd mjpg-streamer/mjpg-streamer-experimental # Go in to the directory that has been downloaded
+
 export LD_LIBRARY_PATH=. # exporting the path to the curent directory
+
 make # making the programa
-echo "${RED}Making webcamDaemon executable ${NC}\n"
-sudo chmod +x /root/mjpg-streamer/scripts/WebcamDaemon
-echo "${RED}installing the raswebcamd service${NC}\n"
-sudo mv /root/mjpg-streamer/Instalation/raswebcamd.service /etc/systemd/system/raswebcamd.service
-sudo systemctl daemon-reload
-sudo systemctl enable raswebcamd
-sudo systemctl start raswebcamd
+
+echo "Making webcamDaemon executable "
+
+sudo chmod +x /root/mjpg-streamer/scripts/WebcamDaemon # Making the WebcamDaemon executable
+
+echo "installing the raswebcamd service"
+
+sudo mv /root/mjpg-streamer/Instalation/raswebcamd.service /etc/systemd/system/raswebcamd.service # Moving the rascamd service to the service folder
+
+sudo systemctl daemon-reload # reloading the daemon to include the raswebcam service
+
+sudo systemctl enable raswebcamd # enableling the raswebcamd service
+
+sudo systemctl start raswebcamd # starting the raswebcamd service
